f08b60b41e8c1dcdac5767249b699f7417e81f6b
root <root@ce-front0.dev.kaltura.com>
2015-03-20 00:24:14 -0400

    New clientlib ver

--- python/KalturaClient/Base.py
+++ python/KalturaClient/Base.py
@@ -264,7 +264,7 @@ class KalturaConfiguration(object):
         self.serviceUrl                 = serviceUrl
         self.partnerId                  = None
         self.format                     = KALTURA_SERVICE_FORMAT_XML
-        self.clientTag                  = "python:15-03-19"
+        self.clientTag                  = "python:15-03-20"
         self.requestTimeout             = 120
         
         if partnerId != None and type(partnerId) != int:
