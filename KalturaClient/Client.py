41a3f50f7db577f4a839709fdea5669f6164dbc1
Kaltura CSI <community@kaltura.com>
2020-07-17 20:20:42 -0400

    New clientlib ver

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-20-07-16',
+            'clientTag': 'python-20-07-17',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
