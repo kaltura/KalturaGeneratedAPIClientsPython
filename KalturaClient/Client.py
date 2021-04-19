fd6b0f6108d6c728d17c9593c29f0892cf00d43d
Kaltura CSI <community@kaltura.com>
2021-04-19 19:29:27 -0400

    New clientlib ver

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-21-04-18',
+            'clientTag': 'python-21-04-19',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
