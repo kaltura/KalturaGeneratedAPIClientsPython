abf2567f202d423a3aa0656053e7a5e3be8992ff
Kaltura CSI <community@kaltura.com>
2022-01-05 20:05:59 -0500

    New clientlib ver

--- KalturaClient/Plugins/EventNotification.py
+++ KalturaClient/Plugins/EventNotification.py
@@ -120,7 +120,6 @@ class KalturaEventNotificationEventObjectType(object):
 # @package Kaltura
 # @subpackage Client
 class KalturaEventNotificationEventType(object):
-    INTEGRATION_JOB_CLOSED = "integrationEventNotifications.INTEGRATION_JOB_CLOSED"
     BATCH_JOB_STATUS = "1"
     OBJECT_ADDED = "2"
     OBJECT_CHANGED = "3"
