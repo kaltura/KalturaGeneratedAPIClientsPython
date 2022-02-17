d9e3543316762477de39e771ea6eb0b0078464fc
Kaltura CSI <community@kaltura.com>
2022-02-16 19:38:44 -0500

    New clientlib ver

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-22-02-15',
+            'clientTag': 'python-22-02-16',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -8,7 +8,7 @@
 # to do with audio, video, and animation what Wiki platforms allow them to do with
 # text.
 #
-# Copyright (C) 2006-2021  Kaltura Inc.
+# Copyright (C) 2006-2022  Kaltura Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '17.18.0'
+API_VERSION = '17.19.0'
 
 ########## enums ##########
 # @package Kaltura
@@ -5368,6 +5368,11 @@ class KalturaReportType(object):
     ENGAGMENT_TIMELINE_WEBCAST = "40011"
     ENGAGEMENT_TOOLS_WEBCAST = "40012"
     REACTIONS_BREAKDOWN_WEBCAST = "40013"
+    VE_HIGHLIGHTS = "50000"
+    VE_REGISTERED_PLATFORMS = "50001"
+    VE_REGISTERED_INDUSTRY = "50002"
+    VE_REGISTERED_ROLES = "50003"
+    VE_REGISTERED_COUNTRIES = "50004"
 
     def __init__(self, value):
         self.value = value
